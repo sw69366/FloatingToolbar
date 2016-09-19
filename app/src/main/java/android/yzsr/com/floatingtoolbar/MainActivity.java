24e49ee9c006b180ffae0401ef9b1e92f1c7ba54
sw69366 <dell3450800>
2016-09-19 14:44:08 +0800

    修改BUG

--- app/src/main/java/android/yzsr/com/floatingtoolbar/MainActivity.java
+++ app/src/main/java/android/yzsr/com/floatingtoolbar/MainActivity.java
@@ -18,12 +18,10 @@ public class MainActivity extends AppCompatActivity {
     RelativeLayout rootView;
     private int naviBarHeight, screenHeight, screenWidth;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         rootView = ((RelativeLayout) findViewById(R.id.rootView));
 
         Rect rect = new Rect();
@@ -43,37 +41,52 @@ public class MainActivity extends AppCompatActivity {
         //这里手动计算工具栏大小
         ViewGroup.LayoutParams layoutParams = floatingToolbar.getLayoutParams();
         layoutParams.width = screenWidth;
-        layoutParams.height = 200;
+        layoutParams.height = (int) (screenWidth * 0.13f);
+
+        floatingToolbar.setLayoutParams(layoutParams);
 
         final View decorView = getWindow().getDecorView();
 
         decorView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            int previousKeyboardHeight = -1;
+
+
             @Override
             public void onGlobalLayout() {
-                Rect rect = new Rect();
-                decorView.getWindowVisibleDisplayFrame(rect);
-                int displayHeight = rect.bottom - rect.top;
-                int height = decorView.getHeight();
-                int keyboardHeight = height - displayHeight;
-                if (previousKeyboardHeight != keyboardHeight) {
-                    boolean hide = (double) displayHeight / height > 0.8;
-                    listener.onSoftKeyBoardChange(keyboardHeight, !hide);
-                }
-                previousKeyboardHeight = height;
+
             }
         });
+
+
+       getWindow().getDecorView().addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+           int previousKeyboardHeight = -1;
+           @Override
+           public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+               Rect rect = new Rect();
+               decorView.getWindowVisibleDisplayFrame(rect);
+               int displayHeight = rect.bottom - rect.top;
+               int height = decorView.getHeight();
+               int keyboardHeight = height - displayHeight;
+               if (previousKeyboardHeight != keyboardHeight) {
+                   boolean hide = (double) displayHeight / height > 0.8;
+                   listener.onSoftKeyBoardChange(keyboardHeight, !hide);
+               }
+               previousKeyboardHeight = height;
+           }
+       });
 
 
         listener = new OnSoftKeyboardChangeListener() {
             @Override
             public void onSoftKeyBoardChange(int softKeybardHeight, boolean visible) {
                 if (visible) {
-                    //显示工具栏
-                    floatingToolbar.setX(0);
-                    //200 对应上部分工具栏高度
-                    floatingToolbar.setY(screenHeight-200-softKeybardHeight-naviBarHeight);
                     floatingToolbar.setVisibility(View.VISIBLE);
+                    int height = floatingToolbar.getLayoutParams().height;
+                    ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) floatingToolbar.getLayoutParams();
+                    lp.topMargin = screenHeight - softKeybardHeight - height + naviBarHeight;
+
+                    floatingToolbar.setLayoutParams(lp);
+
+
                 } else {
                     floatingToolbar.setVisibility(View.GONE);
                 }
@@ -86,11 +99,5 @@ public class MainActivity extends AppCompatActivity {
         void onSoftKeyBoardChange(int softKeybardHeight, boolean visible);
     }
 
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-    }
 
 }
